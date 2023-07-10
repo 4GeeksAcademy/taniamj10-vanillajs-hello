8f942e16322292299c0d42c4552df309eb7c927f
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-07-10 16:43:00 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+for (let pronoun1 = 0; pronoun1 < pronoun.length; pronoun1++) {
+  for (let adj1 = 0; adj1 < adj.length; adj1++) {
+    for (let noun1 = 0; noun1 < noun.length; noun1++) {
+      console.log(pronoun[pronoun1] + adj[adj1] + noun[noun1] + ".com");
+    }
+  }
+}
